18a0233c94b871fa5de79b843fdd4b0a72aa1439
roamerloner <mkhan181046@bscse.uiu.ac.bd>
2024-06-10 03:36:19 +0600

    Seperated router files

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
 require '../helpers.php';
-loadView('home');
+
+
+
+$uri = $_SERVER['REQUEST_URI'];
+
+require basePath('router.php');
 
--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,13 +1,6 @@
 <?php loadPartial('head'); ?>
 <?php loadPartial('navbar'); ?>
-<?php loadPartial('showcase-search'); ?>
 <?php loadPartial('top-banner'); ?>
-
-
-
-
-
-<!-- Job Listings -->
 <section>
     <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
@@ -161,11 +154,8 @@
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
+
 
 <?php loadPartial('bottom-banner'); ?>
 <?php loadPartial('footer'); ?>
