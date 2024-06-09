f58d0debcb144dfd2002107d862c951e8af54dd5
roamerloner <mkhan181046@bscse.uiu.ac.bd>
2024-06-10 02:08:47 +0600

    Adding helper functions and partials.

--- helpers.php
+++ helpers.php
@@ -10,3 +10,37 @@
  function basePath($path = ' '){
     return __DIR__ . '/' . $path;
  }
+
+/**
+ * Load View
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+   $viewPath = basePath("views/{$name}.view.php");
+
+   if(file_exists($viewPath)){
+      require $viewPath;
+   } else{
+      echo "View '{$name} not found!'";
+   }
+}
+
+/**
+ * Load Partials
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+   $partialPath = basePath("views/partials/{$name}.php");
+
+   if(file_exists($partialPath)){
+      require $partialPath;
+   }else {
+      echo "View '{$name} not found!'";
+   }
+}
